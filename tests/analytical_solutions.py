714e1ac412c62189c852a820c0a685e528742bee
Harsh Bishnoi <112414164+harsh2929@users.noreply.github.com>
2023-03-24 12:20:36 +0530

    Updated files and added for better implementation
    Add docstrings to all functions to explain their inputs, outputs, and purpose. This will help other developers (and yourself) understand the code more easily.
    
    Add consistent text types in the code.
    
    increase imports in the code instead of manual addition of functionality
    
    Increase code modularity
    
    Use descriptive variable names that explain what each variable represents.
    
    Added error handling to the code.
    
    Use numpy functions for enhancement of overall functioning.

--- tests/analytical_solutions.py
+++ tests/analytical_solutions.py
@@ -1,94 +1,64 @@
 import numpy as np
-
 from gprMax.constants import c, e0
 from gprMax.waveforms import Waveform
 
 
-def hertzian_dipole_fs(iterations, dt, dxdydz, rx):
+def hertzian_dipole_fs(iterations: int, dt: float, dxdydz: tuple, rx: tuple) -> np.ndarray:
     """Analytical solution of a z-directed Hertzian dipole in free space with a Gaussian current waveform (http://dx.doi.org/10.1016/0021-9991(83)90103-1).
 
     Args:
         iterations (int): Number of time steps.
         dt (float): Time step (seconds).
-        dxdydz (float): Tuple of spatial resolution (metres).
-        rx (float): Tuple of coordinates of receiver position relative to transmitter position (metres).
+        dxdydz (tuple[float, float, float]): Spatial resolution (metres) as a tuple of floats.
+        rx (tuple[float, float, float]): Coordinates of receiver position relative to transmitter position (metres) as a tuple of floats.
 
     Returns:
-        fields (float): Array contain electric and magnetic field components.
+        fields (np.ndarray): Array containing electric and magnetic field components.
     """
 
-    # Waveform
-    w = Waveform()
-    w.type = 'gaussianprime'
-    w.amp = 1
-    w.freq = 1e9
+    # Create waveform objects for the different types of waveforms
+    w = Waveform(type='gaussianprime', amp=1, freq=1e9)
+    wint = Waveform(type='gaussian', amp=w.amp, freq=w.freq)
+    wdot = Waveform(type='gaussiandoubleprime', amp=w.amp, freq=w.freq)
 
-    # Waveform integral
-    wint = Waveform()
-    wint.type = 'gaussian'
-    wint.amp = w.amp
-    wint.freq = w.freq
+    # Create an array to store the electric and magnetic field components at each time step
+    fields = np.zeros((iterations, 6))
 
-    # Waveform first derivative
-    wdot = Waveform()
-    wdot.type = 'gaussiandoubleprime'
-    wdot.amp = w.amp
-    wdot.freq = w.freq
-
-    # Time
-    time = np.linspace(0, 1, iterations)
-    time *= (iterations * dt)
-
-    # Spatial resolution
-    dx = dxdydz[0]
-    dy = dxdydz[1]
-    dz = dxdydz[2]
+    # Unpack the spatial resolution tuple
+    dx, dy, dz = dxdydz
 
     # Length of Hertzian dipole
     dl = dz
 
-    # Coordinates of Rx relative to Tx
-    x = rx[0]
-    y = rx[1]
-    z = rx[2]
-    if z == 0:
-        sign_z = 1
-    else:
-        sign_z = np.sign(z)
+    # Unpack the receiver position tuple
+    x, y, z = rx
+    sign_z = 1 if z == 0 else np.sign(z)  # Check if z == 0 and set the sign of z accordingly
 
-    # Coordinates of Rx for Ex FDTD component
-    Ex_x = x + 0.5 * dx
-    Ex_y = y
-    Ex_z = z - 0.5 * dz
-    Er_x = np.sqrt((Ex_x**2 + Ex_y**2 + Ex_z**2))
+    # Calculate the coordinates and the time delay for each of the FDTD components
+
+    # Ex component
+    Ex_x, Ex_y, Ex_z = x + 0.5 * dx, y, z - 0.5 * dz
+    Er_x = np.sqrt(Ex_x ** 2 + Ex_y ** 2 + Ex_z ** 2)
     tau_Ex = Er_x / c
 
-    # Coordinates of Rx for Ey FDTD component
-    Ey_x = x
-    Ey_y = y + 0.5 * dy
-    Ey_z = z - 0.5 * dz
-    Er_y = np.sqrt((Ey_x**2 + Ey_y**2 + Ey_z**2))
+    # Ey component
+    Ey_x, Ey_y, Ey_z = x, y + 0.5 * dy, z - 0.5 * dz
+    Er_y = np.sqrt(Ey_x ** 2 + Ey_y ** 2 + Ey_z ** 2)
     tau_Ey = Er_y / c
 
-    # Coordinates of Rx for Ez FDTD component
-    Ez_x = x
-    Ez_y = y
-    Ez_z = z
-    Er_z = np.sqrt((Ez_x**2 + Ez_y**2 + Ez_z**2))
+    # Ez component
+    Ez_x, Ez_y, Ez_z = x, y, z
+    Er_z = np.sqrt(Ez_x ** 2 + Ez_y ** 2 + Ez_z ** 2)
     tau_Ez = Er_z / c
 
-    # Coordinates of Rx for Hx FDTD component
-    Hx_x = x
-    Hx_y = y + 0.5 * dy
-    Hx_z = z
-    Hr_x = np.sqrt((Hx_x**2 + Hx_y**2 + Hx_z**2))
+    # Hx component
+    Hx_x, Hx_y, Hx_z = x, y + 0.5 * dy, z
+    Hr_x = np.sqrt(Hx_x ** 2 + Hx_y ** 2 + Hx_z ** 2)
     tau_Hx = Hr_x / c
 
-    # Coordinates of Rx for Hy FDTD component
-    Hy_x = x + 0.5 * dx
-    Hy_y = y
-    Hy_z = z
-    Hr_y = np.sqrt((Hy_x**2 + Hy_y**2 + Hy_z**2))
+    # Hy component
+    Hy_x, Hy_y, Hy_z = x + 0.5 * dx, y, z
+    Hr_y = np.sqrt(Hy_x ** 2 + Hy_y ** 2 + Hy_z ** 2)
     tau_Hy = Hr_y / c
     
     # Coordinates of Rx for Hz FDTD component
@@ -132,7 +102,7 @@ def hertzian_dipole_fs(iterations, dt, dxdydz, rx):
         # Ex
         fields[timestep, 0] = ((Ex_x * Ex_z) / (4 * np.pi * e0 * Er_x**5)) * (3 * (fint_Ex + (tau_Ex * f_Ex)) + (tau_Ex**2 * fdot_Ex))
 
-        # Ey
+        # Ey 
         try:
             tmp = Ey_y / Ey_x
         except ZeroDivisionError:
