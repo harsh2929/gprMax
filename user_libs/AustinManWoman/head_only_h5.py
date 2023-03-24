461f3dae70cfe2b0dce8b8ff8cc912531aacf064
Harsh Bishnoi <112414164+harsh2929@users.noreply.github.com>
2023-03-24 11:18:02 +0530

    Update head_only_h5.py
    Use context managers for file handling.
    Add a main function for better code handling.
    Added f-string formatting

--- user_libs/AustinManWoman/head_only_h5.py
+++ user_libs/AustinManWoman/head_only_h5.py
@@ -3,23 +3,28 @@ import os
 
 import h5py
 
-# Parse command line arguments
-parser = argparse.ArgumentParser(description='Writes a HDF5 file of AustinMan or AustinWoman head only.', usage='python head_only_hdf5 filename')
-parser.add_argument('filename', help='name and path to (HDF5) file containing AustinMan or AustinWoman model')
-args = parser.parse_args()
 
-# Read full body HDF5 file
-f = h5py.File(args.filename, 'r')
-dx_dy_dz = f.attrs['dx_dy_dz']
-data = f['/data'][:, :, :]
+def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Writes a HDF5 file of AustinMan or AustinWoman head only.', usage='python head_only_hdf5 filename')
+    parser.add_argument('filename', help='name and path to (HDF5) file containing AustinMan or AustinWoman model')
+    args = parser.parse_args()
 
-# Define head as last 1/8 of total body height
-nzhead = 7 * int(data.shape[2] / 8)
+    # Read full body HDF5 file
+    with h5py.File(args.filename, 'r') as f:
+        dx_dy_dz = f.attrs['dx_dy_dz']
+        data = f['/data'][:, :, :]
 
-print('Dimensions of head model: {:g} x {:g} x {:g} cells'.format(data.shape[0], data.shape[1], data.shape[2] - nzhead))
+    # Define head as last 1/8 of total body height
+    nzhead = 7 * int(data.shape[2] / 8)
 
-# Write HDF5 file
-headfile = os.path.splitext(args.filename)[0] + '_head.h5'
-f = h5py.File(headfile, 'w')
-f.attrs['dx_dy_dz'] = dx_dy_dz
-f['/data'] = data[:, :, nzhead:data.shape[2]]
+    print(f"Dimensions of head model: {data.shape[0]} x {data.shape[1]} x {data.shape[2] - nzhead} cells")
+
+    # Write HDF5 file
+    headfile = os.path.splitext(args.filename)[0] + '_head.h5'
+    with h5py.File(headfile, 'w') as f:
+        f.attrs['dx_dy_dz'] = dx_dy_dz
+        f['/data'] = data[:, :, nzhead:data.shape[2]]
+
+if __name__ == '__main__':
+    main()
