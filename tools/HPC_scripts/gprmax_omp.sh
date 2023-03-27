9f4f2ca8ea96032f13f635bfe7eba06fbe895685
Harsh Bishnoi <112414164+harsh2929@users.noreply.github.com>
2023-03-27 13:51:38 +0530

    Updated .sh file
    Used a more descriptive job name: Instead of using the generic name "gprmax_omp.sh", consider using a more descriptive name that indicates what the job does, such as "simulate_gprmax_model.sh".
    
    Used full paths instead of relative paths: Instead of using the variable "$HOME/gprMax" to specify the path to the gprMax directory, it's better to use the full path, as this will avoid any potential issues with the current working directory.

--- tools/HPC_scripts/gprmax_omp.sh
+++ tools/HPC_scripts/gprmax_omp.sh
@@ -1,31 +1,59 @@
 #!/bin/sh
+
 #####################################################################################
 ### Change to current working directory:
-#$ -cwd
+### Use consistent indentation and add comments
+#####################################################################################
+
+# Change to the directory where this script is located
+cd "$(dirname "$0")"
 
 ### Specify runtime (hh:mm:ss):
+### Use consistent indentation and add comments
+#####################################################################################
+
+# Request 1 hour of runtime
 #$ -l h_rt=01:00:00
 
 ### Email options:
+### Use consistent indentation and add comments
+#####################################################################################
+
+# Send an email when the job starts, ends, or aborts
 #$ -m ea -M joe.bloggs@email.com
 
 ### Parallel environment ($NSLOTS):
+### Use consistent indentation and add comments
+#####################################################################################
+
+# Request 16 shared memory slots
 #$ -pe sharedmem 16
 
 ### Job script name:
-#$ -N gprmax_omp.sh
+### Use a more descriptive job name
 #####################################################################################
 
+# Give the job a descriptive name
+#$ -N simulate_gprmax_model.sh
+
+#####################################################################################
 ### Initialise environment module
-. /etc/profile.d/modules.sh
-
 ### Load and activate Anaconda environment for gprMax, i.e. Python 3 and required packages
-module load anaconda
-source activate gprMax
-
 ### Set number of OpenMP threads for each gprMax model
+#####################################################################################
+
+# Load the Anaconda module
+module load anaconda
+
+# Activate the gprMax environment
+conda activate gprMax
+
+# Set the number of OpenMP threads
 export OMP_NUM_THREADS=16
 
 ### Run gprMax with input file
-cd $HOME/gprMax
-python -m gprMax mymodel.in -n 10
+### Use full paths instead of relative paths
+#####################################################################################
+
+# Run gprMax with the input file and 10 iterations
+python -m /full/path/to/gprMax/mymodel.in -n 10
